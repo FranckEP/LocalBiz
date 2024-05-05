636731ae6739443bd47560b09605e771dfdb5bb2
j0suesv <99438333+j0suesv@users.noreply.github.com>
2024-05-05 16:06:14 -0500

    sidebar is responsive now

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/js/dist/dropdown'
+import './Sidebar.css'
+
 
 function Sidebar(){
     return (
@@ -9,41 +11,41 @@ function Sidebar(){
             <div className='row'>
                 <div className='bg-dark col-auto col-md-2 min-vh-100 d-flex justify-content-between flex-column'>
                     <div>
-                        <a className='text-decoration-none text-white d-flex align-itemcenter ms-3 mt-2' href="#">
-                            <span className='ms-1 fs-4'>LocalBiz</span>
+                        <a className='text-decoration-none text-white d-non d-sm-inline d-flex align-itemcenter ms-3 mt-2' href="#">
+                            <span className='ms-1 fs-4 d-none d-sm-inline'>LocalBiz</span>
                         </a>
                         <hr className='text-secondary' />
                         <ul className='nav nav-pills flex-column'>
                             <li className='nav-item text-white fs-4'>
                                 <a className='nav-link text-white fs-5' aria-current='page' href="#">
                                     <i className='bi bi-speedometer2'></i>
-                                    <span className='ms-2'>Dashboard</span>
+                                    <span className='ms-3 d-non d-none d-sm-inline'>Dashboard</span>
                                 </a>
                             </li>
                             <li className='nav-item text-white fs-4'>
                                 <a className='nav-link text-white fs-5' aria-current='page' href="#">
                                     <i className='bi bi-table'></i>
-                                    <span className='ms-2'>Orders</span>
+                                    <span className='ms-3 d-none d-sm-inline'>Orders</span>
                                 </a>
                             </li>
                             <li className='nav-item text-white fs-4'>
                                 <a className='nav-link text-white fs-5' aria-current='page' href="#">
                                     <i className='bi bi-grid'></i>
-                                    <span className='ms-2'>Products</span>
+                                    <span className='ms-3 d-none d-sm-inline'>Products</span>
                                 </a>
                             </li>
                             <li className='nav-item text-white fs-4'>
                                 <a className='nav-link text-white fs-5' aria-current='page' href="#">
                                     <i className='bi bi-person-circle'></i>
-                                    <span className='ms-2'>Customers</span>
+                                    <span className='ms-3 d-none d-sm-inline'>Customers</span>
                                 </a>
                             </li>
 
                         </ul>
                     </div>
                     <div className='dropdown open'>
-                        <a className='text-decoration-none text-white dropdown-toggle p-3' type='button' id='triggerId' data-bs-toggle='dropdown' aria-expanded='false'>
-                            <i className='bi bi-person-circle'></i> <span className='ms-2'>LocalBiz</span>
+                        <a className='text-decoration-none text-white dropdown-toggle p-3' type='button' id='triggerId' data-bs-toggle="dropdown" aria-expanded='false'>
+                            <i className='bi bi-person-circle'></i> <span className='ms-2 d-none d-sm-inline'>LocalBiz</span>
                         </a>
                     </div>
                     <div className='dropdown-menu' aria-labelledby='triggerId'>
